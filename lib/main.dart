d5eb43367722746be5acf6279ee38968ed0136c1
jinzhuxue <985-85485@qq.com>
2020-06-09 14:03:37 +0800

    添加master2文件

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget{
       title: "welcome to flutter",
       home:Scaffold(
         appBar: AppBar(
-          title: Text("Hello world")
+          title: Text("Hello world2222")
         ),
         body: Center(
           child: Text(
