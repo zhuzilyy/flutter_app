7f3b83b99410576b78eb6f44b5a1a02bf888359e
jinzhuxue <985-85485@qq.com>
2020-06-09 14:04:22 +0800

    添加master2文件

--- lib/main.dart
+++ lib/main.dart
@@ -11,11 +11,11 @@ class MyApp extends StatelessWidget{
       title: "welcome to flutter",
       home:Scaffold(
         appBar: AppBar(
-          title: Text("Hello world1111111")
+          title: Text("Hello world")
         ),
         body: Center(
           child: Text(
-                  "我是masetr1分支的数据",
+                  "我是masetr分支的数据",
             textAlign: TextAlign.left,
         /*    maxLines: 1,
             overflow: TextOverflow.ellipsis,*/
